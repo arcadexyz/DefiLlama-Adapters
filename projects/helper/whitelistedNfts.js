4c79992092960996de7cd178fcea7d9ae1b4e162
Elliot Shiu <sandbochs@users.noreply.github.com>
2023-03-03 18:16:07 -0800

    add verified collections on Arcade.xyz to DefiLlama nft whitelist

---
+++ projects/helper/whitelistedNfts.js
@@ -0,0 +1,587 @@
+const ethereum = [
+  // Moonbirds Oddities
+  '0x1792a96e5668ad7c167ab804a100ce42395ce54d',
+
+  // Chromie Squiggle
+  '0x059edd72cd353df5106d2b9cc5ab83a52287ac3a',
+
+  // Deadfellaz
+  '0x2acab3dea77832c09420663b0e1cb386031ba17b',
+
+  // Treeverse
+  '0x1b829b926a14634d36625e60165c0770c09d02b2',
+
+  // World of Women Galaxy
+  '0xf61f24c2d93bf2de187546b14425bf631f28d6dc',
+
+  // Rumble Kong League
+  '0xef0182dc0574cd5874494a120750fd222fdb909a',
+
+  // Avastars
+  '0xf3e778f839934fc819cfa1040aabacecba01e049',
+
+  // Bored Ape Kennel Club
+  '0xba30e5f9bb24caa003e9f2f0497ad287fdf95623',
+
+  // Azuki
+  '0xed5af388653567af2f388e6224dc7c4b3241c544',
+
+  // Yat NFT (YATS)
+  '0x7d256d82b32d8003d1ca1a1526ed211e6e0da9e2',
+
+  // PEGZ
+  '0x1eff5ed809c994ee2f500f076cef22ef3fd9c25d',
+
+  // SMOWL
+  '0x03b8d129a8f6dc62a797b59aa5eebb11ad63dada',
+
+  // CyberBrokers
+  '0x892848074ddea461a15f337250da3ce55580ca85',
+
+  // Invisible Friends
+  '0x59468516a8259058bad1ca5f8f4bff190d30e066',
+
+  // Metroverse Genesis
+  '0x0e9d6552b85be180d941f1ca73ae3e318d2d4f1f',
+
+  // Polymorphs (MORPH)
+  '0x1cbb182322aee8ce9f4f1f98d7460173ee30af1f',
+
+  // Killer GF
+  '0x6be69b2a9b153737887cfcdca7781ed1511c7e36',
+
+  // Pixls (PIXL)
+  '0x082903f4e94c5e10a2b116a4284940a36afaed63',
+
+  // Uniswap V3 Positions NFT-V1
+  '0xc36442b4a4522e871399cd717abdd847ab11fe88',
+
+  // AutoGlyphs
+  '0xd4e4078ca3495de5b1d4db434bebc5a986197782',
+
+  // Meebits
+  '0x7bd29408f11d2bfc23c34f18275bbf23bb716bc7',
+
+  // Fluf World
+  '0xccc441ac31f02cd96c153db6fd5fe0a2f4e6a68d',
+
+  // OnChainMonkey
+  '0x960b7a6bcd451c9968473f7bbfd9be826efd549a',
+
+  // Voxel Ville
+  '0x31c823260528ba98e3bf797b2add2302af097392',
+
+  // SupDucks
+  '0x3fe1a4c1481c8351e91b64d5c398b159de07cbc5',
+
+  // ZombieClub Token
+  '0x9c80777cae192e5031c38a0d951c55730ecc3f5e',
+
+  // Le Anime
+  '0x03bebcf3d62c1e7465f8a095bfa08a79ca2892a1',
+
+  // Midnight Breeze (by Dutchtide)
+  '0xd9c036e9eef725e5aca4a22239a23feb47c3f05d',
+
+  // Emblem Vault (Ethereum)
+  '0x82c7a8f707110f5fbb16184a5933e9f78a34c6ab',
+
+  // Ash Chapter One: Carbon
+  '0xb89fe5a72b3a71e585f468c3ff2d1f67df13b256',
+
+  // CryptoPunks
+  '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
+
+  // Wrapped Cryptopunks
+  '0xb7f7f6c52f2e2fdb1963eab30438024864c313f6',
+
+  // Mutant Ape Yacht Club
+  '0x60e4d786628fea6478f785a6d7e704777c86a7c6',
+
+  // VeeFriends
+  '0xa3aee8bce55beea1951ef834b99f3ac60d1abeeb',
+
+  // Doodles
+  '0x8a90cab2b38dba80c64b7734e58ee1db38b8992e',
+
+  // NFT Worlds
+  '0xbd4455da5929d5639ee098abfaa3241e9ae111af',
+
+  // goblintown.wtf
+  '0xbce3781ae7ca1a5e050bd9c4c77369867ebc307e',
+
+  // Cool Cats
+  '0x1a92f7381b9f03921564a437210bb9396471050c',
+
+  // ASM Aifa Genesis
+  '0x26437d312fb36bdd7ac9f322a6d4ccfe0c4fa313',
+
+  // Metahero Universe: Generative Identities
+  '0x6dc6001535e15b9def7b0f6a20a2111dfa9454e2',
+
+  // Damien Hirst - The Currency
+  '0xaadc2d4261199ce24a4b0a57370c4fcf43bb60aa',
+
+  // Mfers
+  '0x79fcdef22feed20eddacbb2587640e45491b757f',
+
+  // Psychedelics Anonymous Genesis
+  '0x75e95ba5997eb235f40ecf8347cdb11f18ff640b',
+
+  // Forgotten Runes Wizards Cult
+  '0x521f9c7505005cfa19a8e5786a9c3c9c9f5e6f42',
+
+  // CyberKongz VX
+  '0x7ea3cca10668b8346aec0bf1844a49e995527c8b',
+
+  // 3Landers
+  '0xb4d06d46a8285f4ec79fd294f78a881799d8ced9',
+
+  // Karafuru
+  '0xd2f668a8461d6761115daf8aeb3cdf5f40c532c6',
+
+  // Loot (for Adventurers)
+  '0xff9c1b15b16263c61d017ee9f65c50e4ae0113d7',
+
+  // Boss Beauties
+  '0xb5c747561a185a146f83cfff25bdfd2455b31ff4',
+
+  // Capsule House
+  '0xfcb1315c4273954f74cb16d5b663dbf479eec62e',
+
+  // KnownOrigin
+  '0xfbeef911dc5821886e1dda71586d90ed28174b7d',
+
+  // Somnium Space Land
+  '0x913ae503153d9a335398d0785ba60a2d63ddb4e2',
+
+  // BEEPLE - SPRING/SUMMER COLLECTION 2021
+  '0xdd012153e008346591153fff28b0dd6724f0c256',
+
+  // BEEPLE - GENESIS COLLECTION
+  '0x12f28e2106ce8fd8464885b80ea865e98b465149',
+
+  // Trevor Jones Art
+  '0x99a86b41272afb8b599bddcbda01ed6135620385',
+
+  // LenderNote
+  '0xd96e4d03420aa33a3fe91f57d03d8ef69de1e863',
+
+  // Deafbeef
+  '0xd754937672300ae6708a51229112de4017810934',
+
+  // Crypto.Chicks
+  '0x1981cc36b59cffdd24b01cc5d698daa75e367e04',
+
+  // Rug Radio - Genesis NFT
+  '0x8ff1523091c9517bc328223d50b52ef450200339',
+
+  // NeoTokyo Outer Identities
+  '0x698fbaaca64944376e2cdc4cad86eaa91362cf54',
+
+  // Mooncats *acclimatedmooncats
+  '0xc3f733ca98e0dad0386979eb96fb1722a1a05e69',
+
+  // Bastard Gan Punks V2
+  '0x31385d3520bced94f77aae104b406994d8f2168c',
+
+  // Mind The Gap by MountVitrivius
+  '0x0e42ffbac75bcc30cd0015f8aaa608539ba35fbb',
+
+  // Murakami.Flowers
+  '0x7d8820fa92eb1584636f4f5b8515b5476b75171a',
+
+  // Adam Bomb Squad
+  '0x7ab2352b1d2e185560494d5e577f9d3c238b78c5',
+
+  // Babbys
+  '0x9a4b320418544b4da92c38d2a6e2de09d5b0287a',
+
+  // Nouns
+  '0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03',
+
+  // Chubbicorns
+  '0xb072114151f32d85223ae7b00ac0528d1f56aa6e',
+
+  // Mad Dog Jones
+  '0xd949a444f29ede73f517942eb4d8e3ccdab25d58',
+
+  // Queen+Kings by HackataoQK
+  '0xbb869f884186c3cba0ffa89ab84980cb86f8744d',
+
+  // Space Doodles
+  '0x620b70123fb810f6c653da7644b5dd0b6312e4d8',
+
+  // Van Minion (CRVM)
+  '0xc9bc23735b5de17c14776f79ac041d12ff72ffd3',
+
+  // RaidParty Heroes
+  '0x966731dfd9b9925dd105ff465687f5aa8f54ee9f',
+
+  // Four Winds by Blake Kathryn (NiftyGateWay)
+  '0xa70d6b6222fcc73dc26512634afe8bde378f3b7e',
+
+  // The Fungible by Pak
+  '0xc0cf5b82ae2352303b2ea02c3be88e23f2594171',
+
+  // Synthetic Dreams by Refik Anadol
+  '0x183368d767b299681fdf660233e39f9f8cf8be3a',
+
+  // Nina’s Super Cool World
+  '0x670d4dd2e6badfbbd372d0d37e06cd2852754a04',
+
+  // Tom Sachs: Rocket Factory - Rockets
+  '0x6b00de202e3cd03c523ca05d8b47231dbdd9142b',
+
+  // Edenhorde
+  '0x9eeaecbe2884aa7e82f450e3fc174f30fc2a8de3',
+
+  // Bullrun Babes
+  '0x4ad4455ad5ef891695c221e8e683efa65fabede0',
+
+  // The title by Pak
+  '0x090c53bac270768759c8f4c93151bd1a808a280e',
+
+  // CloneX
+  '0x49cf6f5d44e70224e2e23fdcdd2c053f30ada28b',
+
+  // Otherdeed for Otherside
+  '0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258',
+
+  // MoonBirds
+  '0x23581767a106ae21c074b2276d25e5c3e136a68b',
+
+  // World of Women
+  '0xe785e82358879f061bc3dcac6f0444462d4b5330',
+
+  // Cool Pets NFT
+  '0x86c10d10eca1fca9daf87a279abccabe0063f247',
+
+  // Neo Tokyo Identities
+  '0x86357a19e5537a8fba9a004e555713bc943a66c0',
+
+  // Pudgy Penguins
+  '0xbd3531da5cf5857e7cfaa92426877b022e612cf8',
+
+  // Beeple: Everydays - The 2020 Collection
+  '0xd92e44ac213b9ebda0178e1523cc0ce177b7fa96',
+
+  // CrypToadz
+  '0x1cb1a5e65610aeff2551a50f76a87a7d3fb649c6',
+
+  // Hackatao
+  '0xb396b8699f728735f28a31279d07b1d0d2411baa',
+
+  // TrevorJonesArt
+  '0xe9be55ffedb6c2a2f3f8eac31c60d7f122f79958',
+
+  // The Carbon Drop
+  '0x0151834a6997f89eb8372ac54ac077d79bb4d1e0',
+
+  // BEEPLE: EVERYDAYS - THE 2020 COLLECTION
+  '0xc170384371494b2a8f6ba20f4d085c4dde763d96',
+
+  // The Collision by Pak x Trevor Jones
+  '0x66b25eda64d507fba4ad0eee4c9498552ff698fd',
+
+  // BorrowerNote
+  '0x9b458e2b9c0cd34a62a26b846f45eb829aebc96e',
+
+  // PROOF Collective
+  '0x08d7c0242953446436f34b4c78fe9da38c73668d',
+
+  // Where My Vans Go
+  '0x509a050f573be0d5e01a73c3726e17161729558b',
+
+  // BYOLand
+  '0x0616a2ef54bad0b37dce41c8d8e35cce17a926f3',
+
+  // Apostles: Genesis
+  '0xe70da20a2b10d60ca620a4e494fe2b37c9499e97',
+
+  // Wassies by Wassies
+  '0x1d20a51f088492a0f1c57f047a9e30c9ab5c07ea',
+
+  // CryptoDickbutts
+  '0x42069abfe407c60cf4ae4112bedead391dba1cdb',
+
+  // BYOCraft
+  '0x3e511fe60d5fe09503c5f2a6477a75d0b905b335',
+
+  // NFTiff
+  '0xe8f88d16f24255fcfab25959705d724406d67d9d',
+
+  // QQL Mint Pass
+  '0xc73b17179bf0c59cd5860bb25247d1d1092c1088',
+
+  // Substrata
+  '0xa4535f84e8d746462f9774319e75b25bc151ba1d',
+
+  // FLUF World: Burrows
+  '0xe51aac67b09eaed6d3d43e794d6bae679cbe09d8',
+
+  // Bufficorn Buidl Brigade
+  '0x1e988ba4692e52bc50b375bcc8585b95c48aad77',
+
+  // Fomoverse
+  '0x74bb71a4210e33256885def483fd4227b7f9d88f',
+
+  // Hedz by Matt Furie
+  '0xefed2a58cc6a5b81f9158b231847f005cf086c01',
+
+  // Bored Ape Yacht Club
+  '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
+
+  // The Potatoz
+  '0x39ee2c7b3cb80254225884ca001f57118c8f21b6',
+
+  // RENGA
+  '0x394e3d3044fc89fcdd966d3cb35ac0b32b0cda91',
+
+  // RENGA Black Box
+  '0x64ad353bc90a04361c4810ae7b3701f3beb48d7e',
+
+  // Steady Stack Titans Official
+  '0x916c6af08bf922eaf80c05975886c0a421c78a35',
+
+  // VeeFriends Series 2
+  '0x9378368ba6b85c1fba5b131b530f5f5bedf21a18',
+
+  // DigiDaigaku Genesis
+  '0xd1258db6ac08eb0e625b75b371c023da478e94a9',
+
+  // Metamorphosis
+  '0x4d232cd85294acd53ec03f4a57f57888c9ea1946',
+
+  // My Head In Your Hands by FEWOCIOUS x Two Feet
+  '0x80a50b716ddc43f525c479925d6aa048604eab14',
+
+  // Wolf Game
+  '0x7f36182dee28c45de6072a34d29855bae76dbe2f',
+
+  //  FEWOCiOUS x FewoWorld: Paint
+  '0x06911466341299d79e9e1368a016c73d009691cc',
+
+  // Light Years by Dmitri Cherniak
+  '0x082dcab372505ae56eafde58204ba5b12ff3f3f5',
+
+  // CyberKongz
+  '0x57a204aa1042f6e66dd7730813f4024114d74f37',
+
+  // Valhalla
+  '0x231d3559aa848bf10366fb9868590f01d34bf240',
+
+  // rektguy
+  '0xb852c6b5892256c264cc2c888ea462189154d8d7',
+
+  // BEEPLE - THE 5000 DAYS COLLECTION
+  '0x23cd7da31501aa54f61384a3756fb4a1329dbb2c',
+
+  // Neo Tokyo Outer Citizen
+  '0x9b091d2e0bb88ace4fe8f0fab87b93d8ba932ec4',
+
+  // Charged Particles - ProtonB V2
+  '0x04d572734006788b646ce35b133bdf7160f79995',
+
+  // Neo Tokyo Citizens
+  '0xb668beb1fa440f6cf2da0399f8c28cab993bdd65',
+
+  // Wolf Game - Genesis Land
+  '0x2c88aa0956bc9813505d73575f653f69ada60923',
+
+  // Wolf Game - Wool Pouch
+  '0xb76fbbb30e31f2c3bdaa2466cfb1cfe39b220d06',
+
+  // Wolf Game - Farmer
+  '0xbda2481db91fc0f942ed3f53de378ba45ba9d17e',
+
+  // Wolf Game Legacy
+  '0xeb834ae72b30866af20a6ce5440fa598bfad3a42',
+
+  // Decentraland
+  '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d',
+
+  // Mutant Hounds by Novel Labs
+  '0x354634c4621cdfb7a25e6486cca1e019777d841b',
+
+  // Checks - VV Edition
+  '0x34eebee6942d8def3c125458d1a86e0a897fd6f9',
+
+  // Arcade.xyz LenderNote
+  '0x349a026a43ffa8e2ab4c4e59fcaa93f87bd8ddee',
+
+  // Friendship Bracelets by Alexis André
+  '0x942bc2d3e7a589fe5bd4a5c6ef9727dfd82f5c8a',
+
+  // Quirkies Originals
+  '0x3903d4ffaaa700b62578a66e7a67ba4cb67787f9',
+
+  // Lil Pudgys
+  '0x524cab2ec69124574082676e6f654a18df49a048',
+
+  // Kuengz Genesis
+  '0x76cc4742f7eaa89a93576505dec37c2c66a76ab7',
+
+  // Genuine Undead
+  '0x209e639a0ec166ac7a1a4ba41968fa967db30221',
+
+  // Art Blocks
+  '0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270',
+
+  // Unisocks
+  '0x65770b5283117639760bea3f867b69b3697a91dd',
+
+  // ENS: Ethereum Name Service
+  '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85',
+
+  // Pudgy Rods
+  '0x062e691c2054de82f28008a8ccc6d7a1c8ce060d',
+
+  // Feral File #009 - Unsupervised by Refik Anadol
+  '0x7a15b36cb834aea88553de69077d3777460d73ac',
+
+  // Flyfish Club
+  '0xc9d8f15803c645e98b17710a0b6593f097064bef',
+
+  // LUSD Chicken Bonds
+  '0xa8384862219188a8f03c144953cf21fc124029ee',
+
+  // RKL Club Mint Pass
+  '0xabb95abfb3d79daf27558e5aff8bf714922ba8db',
+
+  // FLUF World: Thingies
+  '0x1afef6b252cc35ec061efe6a9676c90915a73f18',
+
+  // The Captainz
+  '0x769272677fab02575e84945f03eca517acc544cc',
+
+  // Genesis Box
+  '0xb75f09b4340aeb85cd5f2dd87d31751edc11ed39',
+
+  // PartyBear
+  '0x35471f47c3c0bc5fc75025b97a19ecdde00f78f8',
+
+  // FLUF World: Buzzies
+  '0x2308742aa28cc460522ff855d24a365f99deba7b',
+
+  // Kanpai Pandas
+  '0xacf63e56fd08970b43401492a02f6f38b6635c91',
+
+  // Wonky Stonks
+  '0x518ba36f1ca6dfe3bb1b098b8dd0444030e79d9f',
+
+  // The Sandbox ASSETS
+  '0xa342f5d851e866e18ff98f351f2c6637f4478db5',
+
+  // Happy Goat Genesis
+  '0xd13e468e0a526b5042521b0fd658454242472ea7',
+
+  // Dooplicator
+  '0x466cfcd0525189b573e794f554b8a751279213ac',
+
+  // 9dcc ITERATION-01 looks
+  '0xfd09eb152263488dc5e4654d9f91f0aebee45423',
+
+  // Webaverse Genesis Pass
+  '0x543d43f390b7d681513045e8a85707438c463d80',
+
+  // The Boo Bears
+  '0x2c889a24af0d0ec6337db8feb589fa6368491146',
+
+  // The Band Bears
+  '0xb4e570232d3e55d2ee850047639dc74da83c7067',
+
+  // The Baby Bears
+  '0x9e629d779be89783263d4c4a765c38eb3f18671c',
+
+  // The Bond Bears
+  '0xf17bb82b6e9cc0075ae308e406e5198ba7320545',
+
+  // Bit Bears by Berachain
+  '0x32bb5a147b5371fd901aa4a72b7f82c58a87e36d',
+
+  // Mutant Hound Collars
+  '0xae99a698156ee8f8d07cbe7f271c31eeaac07087',
+
+  // MAX PAIN AND FRENS OPEN EDITION BY XCOPY
+  '0xd1169e5349d1cb9941f3dcba135c8a4b9eacfdde',
+
+  // Kubs
+  '0xeb2dfc54ebafca8f50efcc1e21a9d100b5aeb349',
+
+  // feetpix.wtf
+  '0x5078981549a1cc18673eb76fb47468f546aadc51',
+
+  // SuperRare
+  '0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0',
+
+  // KILLABITS
+  '0x64a1c0937728d8d2fa8cd81ef61a9c860b7362db',
+
+  // Loser Club Official
+  '0x709d30f1f60f03d85a0ef33142ef3259392dc9e1',
+
+  // YOU THE REAL MVP
+  '0x6efc003d3f3658383f06185503340c2cf27a57b6',
+
+  // Smilesss
+  '0x177ef8787ceb5d4596b6f011df08c86eb84380dc',
+
+  // KILLABEARS
+  '0xc99c679c50033bbc5321eb88752e89a93e9e83c5',
+
+  // Sewer Pass
+  '0x764aeebcf425d56800ef2c84f2578689415a2daa',
+
+  // DEGENLAND by King Xerox - Auctions
+  '0x51f26db5f9154c8d426ecd9ebc9709f59bdef202',
+
+  // Akutars
+  '0xaad35c2dadbe77f97301617d82e661776c891fa9',
+
+  // RTFKT Capsule Space Drip
+  '0xd3f69f10532457d35188895feaa4c20b730ede88',
+
+  // Space Drip Forging - RTFKT Space Drip x Nike Air Force 1
+  '0x029af5c555807c2b5d337478e5f895e8f3b557e2',
+
+  // XCOPY
+  '0xfd04e14334d876635e7ea46ae636d4a45d8ffdde',
+
+  // GODA Mint Pass
+  '0x0a36f2178c0db2c85471c45334a1dd17d130fd42',
+
+  // Alien Frens
+  '0x123b30e25973fecd8354dd5f41cc45a3065ef88c',
+
+  // Checks - VV Originals
+  '0x036721e5a769cc48b3189efbb9cce4471e8a48b1',
+
+  // Skulls of Luci
+  '0xc9041f80dce73721a5f6a779672ec57ef255d27c',
+
+  // CryptoPunks V1 (wrapped)
+  '0x282bdd42f4eb70e7a9d9f40c8fea0825b7f68c5d',
+
+  // Opepen Edition
+  '0x6339e5e072086621540d0362c4e3cea0d643e114',
+
+  // Sappy Seals
+  '0x364c828ee171616a39897688a831c2499ad972ec',
+
+  // BEANZ
+  '0x306b1ea3ecdf94ab739f1910bbda052ed4a9f949',
+
+  // Creepz by OVERLORD
+  '0xfe8c6d19365453d26af321d0e8c910428c23873f',
+
+  // Terraforms by Mathcastles
+  '0x4e1f41613c9084fdb9e34e11fae9412427480e56',
+
+  // Momoguro: Holoself
+  '0x59ad67e9c6a84e602bc73b3a606f731cc6df210d',
+];
+
+module.exports = {
+  ethereum,
+}
